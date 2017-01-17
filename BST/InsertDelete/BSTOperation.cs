ff15b9456e7cf3c8f1c6a9fae2440c04505c8672
ashcse <ashcse.khare@gmail.com>
2017-01-17 15:49:12 +0530

    Completed Insert and Inorder functionality

--- BST/InsertDelete/BSTOperation.cs
+++ BST/InsertDelete/BSTOperation.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace BST.InsertDelete
 {
+    /// <summary>
+    /// This class represent a node in Binary search tree.
+    /// </summary>
     public class Node
     {
         public int Data { get; set; }
@@ -13,6 +16,7 @@ namespace BST.InsertDelete
         public Node left { get; set; }
 
         public Node Right { get; set; }
+
     }
 
     /// <summary>
@@ -20,6 +24,74 @@ namespace BST.InsertDelete
     /// </summary>
     public class BST
     {
+        public Node  Root { get; set; }
+
+        #region Private Methods
+
+        /// <summary>
+        /// Inorder traversal
+        /// </summary>
+        /// <param name="root"></param>
+        private void InorderUtil(Node root)
+        {
+            if(root!= null)
+            {
+                InorderUtil(root.left);
+
+                Console.WriteLine(root.Data);
+
+                InorderUtil(root.Right);
+            }
+        }
+
+        /// <summary>
+        /// Utility function which actually inserts node into BST
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        private Node InsertUtil(int key, Node root = null)
+        {
+            if (root == null)
+            {
+                return new Node { Data = key };
+            }
+
+            if (key < root.Data)
+            {
+                root.left = InsertUtil(key, root.left);
+            }
+            else
+            {
+                root.Right = InsertUtil(key, root.Right);
+            }
+
+            // There are no duplicate nodes in BST hence dont do anything if there is any node with given key.
+            return root;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Public function for Inorder Traversal.
+        /// </summary>
+        public void Inorder()
+        {
+            InorderUtil(Root);
+        }
+
+
+        /// <summary>
+        /// Api for inserting a node into BST
+        /// </summary>
+        /// <param name="key"></param>
+        public void Insert(int key)
+        {
+            Root = InsertUtil(key, Root);
+        }
+
         /// <summary>
         /// This method searches a given key in Binary search tree.
         /// </summary>
@@ -28,12 +100,12 @@ namespace BST.InsertDelete
         /// <returns></returns>
         public Node Search(Node root, int key)
         {
-            if((root == null )&&(root.Data == key))
+            if ((root == null) && (root.Data == key))
             {
                 return root;
             }
 
-            if(key < root.Data)
+            if (key < root.Data)
             {
                 return Search(root.left, key);
             }
@@ -43,11 +115,30 @@ namespace BST.InsertDelete
             }
         }
 
+        /// <summary>
+        /// Api to delete a given node from BST.
+        /// </summary>
+        /// <param name="key"></param>
+        public void Delete(int key)
+        {
 
+        }
+
+        #endregion
     }
 
-    class BSTOperation
+    public class BSTTester
     {
-
+        public void Test()
+        {
+            BST bst = new BST();
+            bst.Insert(10);
+            bst.Insert(1);
+            bst.Insert(3);
+            bst.Insert(15);
+            bst.Insert(18);
+            bst.Insert(0);
+            bst.Inorder();
+        }
     }
 }
--- BST/Program.cs
+++ BST/Program.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BST.InsertDelete;
 namespace BST
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            BSTTester tester = new BSTTester();
+            tester.Test();
+            Console.ReadLine();
         }
     }
 }
