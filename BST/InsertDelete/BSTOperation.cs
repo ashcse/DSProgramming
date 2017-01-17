95f7ee56dfd69b70ba2091932ef9315bbabd22f5
ashcse <ashcse.khare@gmail.com>
2017-01-17 15:20:25 +0530

    Added files for BST operation

---
+++ BST/InsertDelete/BSTOperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST.InsertDelete
+{
+    public class Node
+    {
+        public int Data { get; set; }
+
+        public Node left { get; set; }
+
+        public Node Right { get; set; }
+    }
+
+
+    public class BST
+    {
+        
+    }
+
+    class BSTOperation
+    {
+
+    }
+}
