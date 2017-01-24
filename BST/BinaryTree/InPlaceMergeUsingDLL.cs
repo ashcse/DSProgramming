cc4de977f47f3e394be684224db13657ce4c3692
ashcse <ashcse.khare@gmail.com>
2017-01-24 18:21:36 +0530

    Completed in place merge of two balanced binary search. This method converts first into DLLs and then merges them and again creates a balanced BST from merged DLL.

--- BST/BinaryTree/InPlaceMergeUsingDLL.cs
+++ BST/BinaryTree/InPlaceMergeUsingDLL.cs
@@ -32,9 +32,54 @@ namespace BST.BinaryTree
 
             Node mergedDLLList = ConvertBSTToDLL.MergeSortedDll(firstDLLList, secondDLLList);
 
-
-            return null;
+            return InPlaceConverstionFromSortedDLLtoBST(mergedDLLList);         
         }
+
+
+        public static Node InPlaceConverstionFromSortedDLLtoBST(Node sortedDLL)
+        {
+            int count = GetDLLCount(sortedDLL);
+
+            return InPlaceDLLToBSTUtil(ref sortedDLL, count);
+           
+        }
+
+        /// <summary>
+        /// Actual logic of in place DLL to balanced Binary Search tree conversion.
+        /// </summary>
+        /// <param name="dllHead"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public static Node InPlaceDLLToBSTUtil(ref Node dllHead, int count)
+        {           
+            if(count <=0)
+            {
+                return null;
+            }
+
+            Node left = InPlaceDLLToBSTUtil(ref dllHead, count / 2);
+
+            Node root = dllHead;
+            root.left = left;
+            dllHead = dllHead.Right;
+
+            root.Right = InPlaceDLLToBSTUtil(ref dllHead, count - count / 2 - 1);
+            return root;
+        }
+
+        public static int GetDLLCount(Node head)
+        {
+            int count = 0;
+
+            while(head != null)
+            {
+                count++;
+                head = head.Right;
+            }
+
+            return count;
+        }
+
     }
 
 
@@ -66,19 +111,39 @@ namespace BST.BinaryTree
 
             second.left.Right = null;
             second.left = null;
+            // Removed circle already now use simple while loop instead of do while loop.
 
+            Node head1 = first;
+            Node head2 = second;
            
             Node left = first;
             Node right = second;
-            
+
+            Node resultHead = new Node();
+            Node tail = resultHead.Right;
+
             // Below logic merges both sorted linked lists into first doubly linked list
             // if first list's element is less than second list then it keeps traversing until it finds element which is greater then second element
             // of until list finishes. It then inserts element before current item (item on which cursor is).
             // Hence this logic only inserts element from second list into first list before element which algorithm selects.
-            do
+            while ((right != null) && (left != null)) 
             {
                 if (left.Data < right.Data)
                 {
+                    if (resultHead.Right == null)
+                    {
+                        resultHead.Right = left;                        
+                    }
+                    else
+                    {
+                        tail.Right = left;
+                        left.left = tail;
+                    }
+
+                    tail = left;
+                    left = left.Right;
+
+                    /*
                     while ((left.Data < right.Data) && (left != first))
                     {
                         left = left.Right;
@@ -95,10 +160,25 @@ namespace BST.BinaryTree
                     
                     left = left.Right;
                     right = temp;
-
+                    */
                 }
                 else
                 {
+                    if(resultHead.Right == null)
+                    {
+                        resultHead.Right = right;                       
+                    }
+                    else
+                    {
+                        tail.Right = right;
+                        right.left = tail;
+
+                    }
+
+                    tail = right;
+                    right = right.Right;
+
+                    /*
                     Node rightRight = right.Right;
                     Node prevleft = left.left;
                     left.left = right;
@@ -108,26 +188,27 @@ namespace BST.BinaryTree
 
 
                     right = rightRight;
+                    */
                 }
             }
-            while ((right != second) && (left!= first));
 
-            left = first;
-
-            while(right != second)
+            while(left != null)
             {
-                Node rightRight = right.Right;
-                Node prevleft = left.left;
-                left.left = right;
-                right.Right = left;
-                prevleft.Right = right;
-                right.left = prevleft;
-
-
-                right = rightRight;
+                tail.Right = left;
+                left.left = tail;
+                tail = left;
+                left = left.Right;
             }
 
-            return null;
+            while(right != null)
+            {
+                tail.Right = right;
+                right.left = tail;
+                tail = right;
+                right = right.Right;
+            }
+
+           return resultHead.Right;
         }
 
         /// <summary>
